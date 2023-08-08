c883b348fb75d7623c788aab6fec3e6abf10e91f
mohamedhussammm <mohamed2110201@miuegypt.edu.eg>
2023-08-09 01:29:56 +0300

    .

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from django.shortcuts import redirect
 def home(request):
     return render(request,'homepage.html')
+
+
+def admin_redirect(request):
+    return redirect('/admin')
