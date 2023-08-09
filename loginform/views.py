98244769ce37934d23d66ef7b3424364f03ff5a8
mohamedhussammm <mohamed2110201@miuegypt.edu.eg>
2023-08-10 02:00:52 +0300

    work

--- loginform/views.py
+++ loginform/views.py
@@ -2,18 +2,32 @@ from imaplib import _Authenticator
 from django.shortcuts import redirect, render
 from .models import Login
 from register.models import signuppage
-
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 def index(request):
     if request.method=="POST":
-    
-     username=request.POST.get("username")
+     global username, password
+     username = request.POST.get("username")
      password=request.POST.get("password")
-     res = signuppage.objects.filter(username = username, password = password)
-     print(res.count()>0)
-     if res.count()>0:
-        return render(request,'home/')        
+     user = authenticate(request, username=username, password=password)
+     print(username)
+     print(password)
+     if user is not None:
+      login(request, user)
+      return redirect('/')  
+     else:
+      return render(request,'loginform/index.html')
+    else:
+      return render(request,'loginform/index.html')
+    
+def logout_view(request):
+    logout(request)
+    return redirect('register')
 
+    #  res = signuppage.objects.filter(username = username, password = password)
+    #  print(res.count()>0)
+    #  if res.count()>0:
+        # return render(request,'home/')  
+           
     #  data=Login(username=username,password=password)
     #  data.save()
-    
-    return render(request,'loginform/index.html')
--- register/views.py
+++ register/views.py
@@ -20,10 +20,15 @@ def signup(request):
     nationalid=request.POST.get("nationalid")
     picture=request.POST.get("picture")
     
+    user = User.objects.create_user(username=username, email=email, password=password)
+    user.first_name = firstname
+    user.last_name = lastname
+    user.save()
+
     data= signuppage (firstname=firstname,lastname=lastname,phonenumber=phonenumber,email=email,password=password,cpassword=cpassword,nationalid=nationalid,picture=picture,username=username)
     data.save()
 
- return render (request,'register/signup.html') 
+ return render (request,'register/signup.html')
  
 
 
