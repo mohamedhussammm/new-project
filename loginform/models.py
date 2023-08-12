0b586287ca86e2123cab85a381a0a3fdbcc78d8b
mohamedhussammm <mohamed2110201@miuegypt.edu.eg>
2023-08-12 19:02:34 +0300

    .

--- loginform/models.py
+++ loginform/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-class Login(models.Model):
-    username=models.CharField(max_length=10)
-    password=models.CharField(max_length=20)
-    def __str__(self):
-        return self.username
+
      
  
