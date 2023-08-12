d5a5ace7a63765c365d76976a5fddfe5eb38763a
mohamedhussammm <mohamed2110201@miuegypt.edu.eg>
2023-08-12 19:03:23 +0300

    .

--- register/views.py
+++ register/views.py
@@ -4,6 +4,7 @@ from imaplib import _Authenticator
 from django .http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 def signup(request):
  
@@ -19,11 +20,15 @@ def signup(request):
     cpassword=request.POST.get("cpassword")
     nationalid=request.POST.get("nationalid")
     picture=request.POST.get("picture")
-    
-    user = User.objects.create_user(username=username, email=email, password=password)
-    user.first_name = firstname
-    user.last_name = lastname
-    user.save()
+    if signuppage.objects.filter(email=email).exists():
+       messages.success(request, 'Email already exists.')
+       return render(request,'register/signup.html')
+    else:
+        
+      user = User.objects.create_user(username=username, email=email, password=password)
+      user.first_name = firstname
+      user.last_name = lastname
+      user.save()
 
     data= signuppage (firstname=firstname,lastname=lastname,phonenumber=phonenumber,email=email,password=password,cpassword=cpassword,nationalid=nationalid,picture=picture,username=username)
     data.save()
