4f709403cf4e346a05385de678957ded3eaff3d8
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2023-08-13 04:10:57 +0300

    Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import items,drinks,lunch,dinner,composes
+from .models import Checkout
 
 admin.site.register(items)
 admin.site.register(drinks)
 admin.site.register(lunch)
 admin.site.register(dinner)
 admin.site.register(composes)
-
+admin.site.register(Checkout)
 
 
 
